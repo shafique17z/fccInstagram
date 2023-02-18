75ec8c29c8fce59fec17de79da7a61bdb511113d
Shafique Ahmed <shafiqued17z@gmail.com>
2023-02-18 21:27:54 +0500

    Set up Firebase successfully and integrated it w/ the Project

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LandingScreen from "./app/components/auth/Landing";
+import Constants from "expo-constants";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHUZIsTXn2wUQKfZ-y_VIvrh6ai7nkPWM",
+  authDomain: "fcc-instagram-b80dd.firebaseapp.com",
+  projectId: "fcc-instagram-b80dd",
+  storageBucket: "fcc-instagram-b80dd.appspot.com",
+  messagingSenderId: "316311931387",
+  appId: "1:316311931387:web:6e0f35ae264bfcceae235e",
+  measurementId: "G-VBQKYEMQSH",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +27,7 @@ export default function App() {
       {/* Inside navigation container we will use Stack Navigators */}
       <Stack.Navigator initialRouteName="Landing">
         {/* This will be our initial or first screen of our app hence the name Landing! */}
-        <Stack.Screen name="Landing" component={LandingScreen}/>
+        <Stack.Screen name="Landing" component={LandingScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
