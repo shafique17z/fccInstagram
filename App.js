ef0507108fdb576ca9293b50473a8c65dd941715
Shafique Ahmed <shafiqued17z@gmail.com>
2023-02-18 18:58:00 +0500

    Adding navigation to move between screens using React Navigation library

--- App.js
+++ App.js
@@ -2,12 +2,19 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LandingScreen from "./app/components/auth/Landing";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    //The whole app needs to be inside a navigation container which manages navigation b/w routes
     <NavigationContainer>
+      {/* Inside navigation container we will use Stack Navigation as I have previously done */}
+      <Stack.Navigator initialRouteName="Landing">
+        {/* This will be our initial or first screen in the Stack */}
+        <Stack.Screen name="Screen" component={LandingScreen}/>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
--- app/components/auth/Landing.js
+++ app/components/auth/Landing.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-function Landing(props) {
+function Landing({navigation}) {
   return (
     <View style={styles.container}>
-      
+      <Button 
+        title='Register'
+        onPress={() => navigation.navigate('Register')}
+        />
+      <Button 
+        title='Login'
+        onPress={() => navigation.navigate('Login')}
+        />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
 });
 
 export default Landing;
