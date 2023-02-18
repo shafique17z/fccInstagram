5786c2c56f3a8c1b188be1f852a555a72108a701
Shafique Ahmed <shafiqued17z@gmail.com>
2023-02-18 18:26:34 +0500

    Auth landing page

---
+++ app/components/auth/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+function Landing(props) {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default Landing;
